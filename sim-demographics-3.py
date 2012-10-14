e5bfe4cd7ebddeb9f8488688afa5dd03025d61e4
peter <petrel.harp@gmail.com>
2012-10-13 19:05:58 -0700

    scenario 3

---
+++ sim-demographics-3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+'''Parameters for migration between expanding populations
+   Two populations:
+          No contact since splittime.
+          a and b are similar sized, constant.
+
+   Run with e.g.
+    ( BFIX=$(printf %05d $RANDOM); time nice -19 python ~/projects/coalpedigree/sim-ibd-pedigree.py -b ${BFIX}-growing-migration-3.fibd.gz -l ${BFIX}-growing-migration-3.log -t 150 -i ~/projects/coalpedigree/sim-demographics-3.py )&
+
+'''
+import math
+
+sampsizes = dict( zip(['a','b'],[500,500]) )
+
+# smaller Ne and smaller chromosomes
+splittime = 30
+nesize = 3000000
+
+def ancnefn(pop,t):
+    ancne = dict( a=nesize, b=nesize )
+    return ancne
+
+def migprobs(pop,t):
+    if t>splittime:
+        migprobs = {}.fromkeys( [('a','b'), ('b','a')], .005  )
+    else:
+        migprobs = {}.fromkeys( [('a','b'), ('b','a')], 0.0  )
+    return migprobs
+
